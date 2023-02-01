e20983988e8de4fe0288d890ff2e8b6b644fc6bb
David Vaquero <pepesan@gmail.com>
2023-02-01 15:54:32 +0100

    principio de actualizacion de dependencias a tomca10

--- src/main/java/com/cursosdedesarrollo/app/JdbcServlet.java
+++ src/main/java/com/cursosdedesarrollo/app/JdbcServlet.java
@@ -1,12 +1,13 @@
 package com.cursosdedesarrollo.app;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.h2.jdbc.JdbcSQLSyntaxErrorException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
